5c4d77f5404a901cc2b6d7234587b05a08e191b6
yfei1 <yfei816@gmail.com>
2018-07-05 23:18:24 -0700

    Refine Form logic

--- src/containers/search_bar.jsx
+++ src/containers/search_bar.jsx
@@ -1,11 +1,34 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      term: ''
+    };
+
+  }
+
+  onTermChange = (event) => {
+    this.setState({ term: event.target.value});
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+  }
 
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          placeholder="Get a five-day forcast in your favorite cities!"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onTermChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
       </form>
     );
   }
